acf8ace2107e4455179f97d69705cc3f7898f724
iwirsing <ivymolina@gmail.com>
2023-02-16 18:35:48 -0600

    add webpack

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,7 +24,22 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+//what is this doing vs asset caching
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  //filter requests that we want to cache (js,css)
+  ({ requests}) => ['style','script','worker'].includes(request.destination), 
+  new StaleWhileRevalidate({
+    //cache storage for asset
+    cacheName:'asset-cache',
+    plugins: [
+      //caching to max age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0,200],
+      }),
+    ],
+  }) 
+
+);
